ea91b388b3de37ef78797a04c296940937c7ebce
Azran Hossain <mr69751@gmail.com>
2022-11-27 14:46:29 +0600

    Routers implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./components/Auth/Login";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Registration from "./components/Auth/RegistrationFarmers";
+import Cart from "./components/Cart";
 
 function App() {
-  return <div className="text-3xl">Hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/registration" element={<Registration />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
